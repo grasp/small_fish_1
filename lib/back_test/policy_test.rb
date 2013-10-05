2d4097c46c5dbf14798ed704c31043bc1e1f83ec
hunter <hunter.wxhu@gmail.com>
2013-10-05 21:26:31 +0800

    recover policy_test.rb file from recycle

--- lib/back_test/policy_test.rb
+++ lib/back_test/policy_test.rb
@@ -6,7 +6,7 @@ lib_path=Pathname.new(__FILE__).parent.parent
 require File.join(lib_path,"init","redis_init.rb")
 require File.join(lib_path,"init","stock_list_init.rb")
 require File.join(lib_path,"yahoo","load_daily_k_to_hash.rb")
-require File.join(lib_path,"policy","policy.rb")
+require File.join(lib_path,"back_test","policy.rb")
 require File.join(lib_path,"analysis","K_daily_MACD_analysis.rb")
 require File.join(lib_path,"analysis","price_analysis.rb")
 
@@ -18,7 +18,7 @@ require File.join(lib_path,"analysis","price_analysis.rb")
 #symbol Yahoo股票代码
 
 #
-def back_test_one_stock(backdays,symbol,duration)
+def back_test_one_stock_with_policy(backdays,policy,symbol,duration)
 
    #记录买卖点
 	buy_hash=Hash.new
@@ -26,7 +26,7 @@ def back_test_one_stock(backdays,symbol,duration)
    #记录盈利
 	total_win=0
    #已经买卖的标志
-	last_buy_flag=false
+  last_buy_flag=false
   last_sell_flag=true #初始是卖出状态
 
   #上一次买卖的价格
@@ -38,14 +38,26 @@ def back_test_one_stock(backdays,symbol,duration)
 
   #当前最新价格
   latest_price=0
+  last_keeped_backday=0
+
+  first_buy_flag=true
+  first_buy_price=0
 
   last_macd_signal=Hash.new #存储昨天的MACD，用来判断买入和卖出
 
   #操作记录Hash
   oper_hash=Hash.new
+  oper_hash["policy_buy"] =policy.to_s
+  oper_hash["back_days"] =backdays
+  oper_hash["buy_signal"]=0
+  oper_hash["buy_action"]=0
 
-  oper_record_file=File.new("#{symbol}_oper.txt","w")
+  oper_hash["sell_signal"]=0
+  oper_hash["sell_action"]=0
+  oper_hash["keep_times"]=0
+  
 
+  
   #从倒数的那天开始，计算每天的MACD数组，放发生MACD翻转时候，可以作为买卖信号
   backdays.downto(0).each do |back_days|
   data_hash = yahoo_get_raw_data_from_file(symbol)
@@ -53,9 +65,13 @@ def back_test_one_stock(backdays,symbol,duration)
   #记录操作的文件，用于研究和改进操作点
   #oper_record_file = File.new(File.join(Pathname.new(__FILE__).parent.parent,"report","#{symbol}_oper.txt"),"w") #need record on report folder
   return [symbol,0,nil] if data_array.size ==0
+  return [symbol,0,nil] if data_array.size<backdays
 
   latest_price=data_array[0][1][0]||0
-
+  #按照当日均价来成交，那么均线按照最低价来做是否合理呢？
+ # todays_price= (data_array[back_days][1][1].to_f+data_array[back_days][1][2].to_f)/2
+ #那就按照最低价来操作
+  todays_price= data_array[back_days][1][2].to_f
  #不处理那些没有K线数据的，下载问题
  
 
@@ -70,57 +86,56 @@ def back_test_one_stock(backdays,symbol,duration)
   price_singal=price_signal(data_hash,20)#20 days price signal
 
   #这个地方是买点信号
-  #这个策略只有33%的盈利概率
- #if (macd_signal["5_days_cross_20_days"] ==true && last_macd_signal["5_days_cross_20_days"] ==false && macd_signal["10_days_down_20_days"] ==true &&  macd_signal["20_days_down_30_days"] ==true 	&& macd_signal["30_days_down_60_days"] ==true)
+ # puts " current=#{macd_signal["5_days_cross_10_days"]},last=#{last_macd_signal["5_days_cross_10_days"]},result=#{ macd_signal["5_days_cross_10_days"] && last_macd_signal["5_days_cross_10_days"]}"
+  
+ # if macd_signal["5_days_cross_10_days"]==true && last_macd_signal["5_days_cross_10_days"]==false
+   if calculate_macd_policy(macd_signal,last_macd_signal,policy)
 
-  #这个策略只有22%的盈利概率, 比上面好点
- # if (macd_signal["5_days_cross_10_days"] ==true && last_macd_signal["5_days_down_10_days"] ==false && last_macd_signal["10_days_down_20_days"] ==true &&  last_macd_signal["20_days_down_30_days"] ==true   && last_macd_signal["30_days_down_60_days"] ==true)
+  oper_hash["buy_signal"]+=1
 
-  #这个策略只有%的盈利概率, 比上面好点
- #if macd_signal["5_days_cross_10_days"] == true && macd_signal["5_days_cross_20_days"] == true  && macd_signal["5_days_cross_20_days"] == true && macd_signal["5_days_cross_30_days"] == true   && macd_signal["5_days_cross_60_days"] == true && last_macd_signal["5_days_cross_20_days"] == false
-
-if macd_signal["5_days_cross_10_days"] ==true && last_macd_signal["5_days_down_10_days"]==true
-#if macd_signal["5_days_cross_10_days"] ==true && macd_signal["10_days_down_20_days"]==true && last_macd_signal["5_days_cross_10_days"]==false
- #if macd_signal["5_days_cross_10_days"] ==true
- # && macd_signal["5_days_cross_20_days"] ==false\
- # && macd_signal["10_days_cross_20_days"] ==true\
- # && macd_signal["20_days_cross_30_days"] ==true\
- # && last_macd_signal["5_days_down_10_days"] ==true)
-
-
- # puts "Buy signal"
   #买入操作
   #操作记录，何时买进，
   if last_sell_flag==true && last_buy_flag == false  #确保当下已经卖出股票了，而不是追加投入
 
-  #  puts "buy now!"
-     oper_hash[symbol]=["buy",data_array[back_days][0],data_array[back_days][1][0]]
-     oper_record_file << ["buy",data_array[back_days][0],data_array[back_days][1][0]].to_s
-  	# buy_hash[data_array[back_days][0]]=data_array[back_days][1][0]
+     oper_hash["buy_action"]+=1
+     if first_buy_flag==true
+     first_buy_price= todays_price
+     first_buy_flag=false
+     end
+     oper_hash["buy_"+"#{data_array[back_days][0]}"]=todays_price
+
     #买入价格
-  	 last_buy_price=data_array[back_days][1][0]
+  	 last_buy_price=todays_price
   	 last_buy_flag=true
      last_sell_flag=false
-    # puts "last_buy_flag=#{last_buy_flag},last_sell_flag=#{last_sell_flag}"
+
+     last_keeped_backday =back_days
     end #end of if last_sell
   end #end of if macd_signal....
 
   #这个地方是卖点信号
-   if macd_signal["5_days_down_10_days"]==true 
- # if (macd_signal["10_days_down_10_days"]==true && last_macd_signal["5_days_down_10_days"] ==false)
-  # if macd_signal["5_days_down_20_days"]==true && last_macd_signal["5_days_down_20_days"] ==false
+  #if (macd_signal["5_days_down_10_days"]==true && last_macd_signal["5_days_down_10_days"] ==false)
+   if macd_signal["10_days_cross_20_days"]==true && last_macd_signal["10_days_cross_20_days"] ==false
   #卖出操作
   #统计持仓时间，何时卖出
- # puts "sell signal"
+   oper_hash["sell_signal"]+=1
+   
   if last_buy_flag==true && last_sell_flag ==false
-     # puts "sell  now!"
-     # oper_hash[symbol]=["sell",data_array[back_days][0],data_array[back_days][1][0]]
-      oper_record_file << ["sell",data_array[back_days][0],data_array[back_days][1][0]].to_s
-      #利润计算
-  		total_win+=data_array[back_days][1][0].to_f-last_buy_price.to_f
-  		last_buy_flag=false
-  		last_sell_flag=true
-     # puts "last_buy_flag=#{last_buy_flag},last_sell_flag=#{last_sell_flag}"
+      oper_hash["sell_action"]+=1
+      keep_day=last_keeped_backday-back_days
+      oper_hash["keep_times"]+= keep_day
+      oper_hash["keep_percent"]=((oper_hash["keep_times"].to_f/backdays)*100).to_s+"%"
+
+      win_profit=todays_price-last_buy_price.to_f
+      win_percent=((win_profit.to_f/last_buy_price.to_f)*100).round(2).to_s+"%"
+      total_win+=win_profit
+      #oper_hash[symbol]=["sell",data_array[back_days][0],data_array[back_days][1][0]]
+      oper_hash["sell_"+"#{data_array[back_days][0]}"]=data_array[back_days][1][0]+" keep:#{ keep_day} days,"+"win_percent #{win_percent}"
+       #利润计算
+ 
+  	 last_buy_flag=false
+  	 last_sell_flag=true
+
   end# end of if last_buy_flag
   end#end of if macd_signal
   #记录上次的数据，用于判断
@@ -133,9 +148,33 @@ if last_buy_flag==true
 	total_win+=latest_price.to_f-last_buy_price.to_f
 end
 
-oper_record_file << "total-win=#{total_win}"
-oper_record_file.close #关闭交易记录文件
-puts "#{symbol} done!"
+total_win_percent=((total_win.to_f/first_buy_price.to_f)*100).round(2)
+oper_hash["policy_buy"]+=",win_percent:#{total_win_percent}%"
+mark=String.new
+
+if total_win_percent<=0
+mark=":("*10
+elsif total_win_percent>0 && total_win_percent<10
+mark=""
+elsif total_win_percent>=10 && total_win_percent<20
+mark="**"*10
+elsif total_win_percent>=20 && total_win_percent<30
+mark="**"*20
+elsif total_win_percent>=30
+mark=":-)"*30
+end
+
+puts "#{policy},win_percent=#{total_win_percent}%"
+oper_hash["total-win"] = total_win.to_s+" "+"_win percent=#{total_win_percent}%,"+mark+"\n"
+
+if false
+oper_record_file=File.new("#{symbol}_oper.txt","a+")
+oper_hash.each do |key,value|
+	oper_record_file << "#{key}"+"  "+"#{value}"+"\n"
+end
+oper_record_file.close
+end
+#puts "#{symbol} done!"
 return [symbol,total_win,last_buy_price.to_f]
 
 end
@@ -149,6 +188,8 @@ def win_array_statistic(win_array)
     total_size=temp_array.size
     loss_total=0
     puts temp_array.select {|count| count >0}.size
+    average_win=0
+    temp_array.each { |count| average_win+=count}
 
     win_array_hash["loss_total"]=(((temp_array.select {|count| count <=0}.size)/total_size.to_f).to_f*100).round(2).to_s+"%"
     win_array_hash["loss_100"]=(((temp_array.select {|count| count <=-100}.size)/total_size.to_f).to_f*100).round(2).to_s+"%"
@@ -164,7 +205,7 @@ def win_array_statistic(win_array)
     win_array_hash["win_30_50"]=(((temp_array.select {|count| count>30 && count <=50}.size)/total_size.to_f).to_f*100).round(2).to_s+"%"
     win_array_hash["win_50"]=(((temp_array.select {|count| count>50}.size)/total_size.to_f).to_f*100).round(2).to_s+"%"
     win_array_hash["win_total"]=(((temp_array.select {|count| count>=4}.size)/total_size.to_f).to_f*100).round(2).to_s+"%"
-
+    win_array_hash["average_win"]=((average_win.to_f/total_size.to_f)).round(2).to_s+"%"
    puts temp_array
    puts "total stock number=#{temp_array.size}"
    win_array_hash.each do |key,value|
@@ -178,13 +219,13 @@ end
 ###############################################################
 ##   选取开始和结束的股票样本，可以控制测试的时间，最长的200天全部测试大约需要40分钟
 ##
-def back_test_multi_stock(start,stop)
+def back_test_multi_stock(start,stop,policy)
  
   win_array=Array.new
   start_time = Time.now
 
   #记录每一只股票的盈利记录
-  win_file=File.new("win_list.txt","w")
+  win_file=File.new("win_list.txt","a+")
 
   count =0
 
@@ -193,7 +234,7 @@ def back_test_multi_stock(start,stop)
    if (count >= start && count < stop)
    puts count.to_s + "_" + symbol
 
-   result=back_test_one_stock(200,symbol,0)
+   result=back_test_one_stock_with_policy(120,policy,symbol,0)
 
    next if (result[2]==0 || result[1].nil? || result[2].nil?)   
    percent=((result[1]/result[2])*100).to_f.round(2)
@@ -205,24 +246,63 @@ def back_test_multi_stock(start,stop)
    count+=1
   
   end  # end of $all_stock_list
-  win_file.close 
 
+  win_file<<policy.to_s
+ 
+  win_file<<win_array_statistic(win_array).to_s
    
   #统计盈利分布
-  win_array_statistic(win_array)
-
+  #win_array_statistic(win_array)
+  win_file.close 
   puts "cost time =#{Time.now - start_time}"
   return win_array
+end
+
+
+def test_one_stock
+policy_array=Array.new
+#上穿10日均线，第一个开叉信号
+policy_array << [["5_days_cross_10_days"=>true],["5_days_cross_10_days"=>false]]
+#上穿20日均线-失败
+#policy_array << [["5_days_cross_20_days"=>true],["5_days_cross_20_days"=>false]]
+#上穿30日均线-失败
+#policy_array << [["5_days_cross_30_days"=>true],["5_days_cross_30_days"=>false]]
+#上穿60日均线-失败
+#policy_array << [["5_days_cross_60_days"=>true],["5_days_cross_60_days"=>false]]
+#10日线穿20日线的时候
+policy_array << [["5_days_cross_10_days"=>true,"10_days_cross_20_days"=>true],[]]
+
+#第三个开叉的时候，更安全，买点少
+policy_array << [["5_days_cross_10_days"=>true,"10_days_cross_20_days"=>true,"20_days_cross_30_days"=>true],[]]
+
+stock_array=["000040.sz","000047.sz","000042.sz","000043.sz","000044.sz","000045.sz","000046.sz"]
+stock_array.each do |stock|
+	puts stock
+policy_array.each do |policy|
+  back_test_one_stock_with_policy(60,policy,stock,0)
+end
+end
+end
+
+def test_multi_stock
+policy_array=Array.new
+
+policy_array << [["5_days_cross_10_days"=>true],["5_days_cross_10_days"=>false]]
+policy_array << [["5_days_cross_10_days"=>true,"20_days_down_30_days"=>true],[]]
+#第三个开叉的时候，更安全，买点少
+policy_array << [["5_days_cross_10_days"=>true,"10_days_cross_20_days"=>true,"20_days_cross_30_days"=>true],[]]
+#全部开叉的时候，更安全，买点少
+policy_array << [["5_days_cross_10_days"=>true,"10_days_cross_20_days"=>true,"20_days_cross_30_days"=>true,"30_days_cross_60_days"=>true],[]]
+
+
+policy_array.each do |policy|
+	back_test_multi_stock(1800,1810,policy)
+end
 end
 
 ##################################################
 #below is self test area
 if $0==__FILE__
-
-#back_test_all_stock
-
-#test_win_array=[-200,-100,-90,-80,-70,-60,-50,-40,-30,-20,-10,-5,-3,-2,-1,0,1,2,3,10,20,30,40,50,60,70,80,90,100,200,300]
-#win_array_statistic(test_win_array)
-
-back_test_multi_stock(500,700)
+#test_one_stock
+test_multi_stock
 end
